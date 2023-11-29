5dd8f62c6699358e26d7fda3084031273e48738b
WagaoCarvalho <wagaocarvalho@gmail.com>
2023-11-29 10:18:20 -0300

    refactor: validations controllers/model/user_request.go

--- src/controllers/createUser.go
+++ src/controllers/createUser.go
@@ -2,11 +2,20 @@ package controllers
 
 import (
 	resterrors "crudgo/src/configurations/rest_errors"
+	"crudgo/src/controllers/model/request"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := resterrors.NewBadRequestError("Chamada de rota errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := resterrors.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
